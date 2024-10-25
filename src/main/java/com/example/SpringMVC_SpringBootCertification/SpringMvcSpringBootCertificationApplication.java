4044e03e5d0386af19d1d0a88e7973b0e23e1365
joaopfmonteiro <joaoosverdes@gmail.com>
2024-10-25 17:38:16 +0100

    First commit

---
+++ src/main/java/com/example/SpringMVC_SpringBootCertification/SpringMvcSpringBootCertificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVC_SpringBootCertification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSpringBootCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcSpringBootCertificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringMVC_SpringBootCertification/SpringMvcSpringBootCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVC_SpringBootCertification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSpringBootCertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
